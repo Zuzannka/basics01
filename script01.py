e015fbd01bcd24e365c0a36b7cd29626cce62714
Zuzanka <iglewskaz@gmail.com>
2020-04-02 21:57:57 +0200

    homework upgrated

--- script01.py
+++ script01.py
@@ -1,12 +1,12 @@
 print("Sztuczka magiczna, podaj 3 cyfry i ci wyrzuci coś niepspodzianka")
 a = input('Podaj pierwszą liczbę:')
 b = input('Podaj drugą liczbę:')
-c = bool(int(input("by dodać wpisz 1, by odjąć wpisz 0: ")))
-def funkcja(a,b,c):
-    if c == False:
+option = bool(int(input("by dodać wpisz 1, by odjąć wpisz 0: ")))
+def calculate(a,b,option):
+    if option == False:
         sum = float(a) + float(b)
         print (sum)
     else:
         dif = float(a) - float(b)
         print (dif)
-funkcja(a,b,c)
+calculate(a,b,option)
