9e5f972f00a53a77c7612e501114b21de1f838a9
Zuzanka <iglewskaz@gmail.com>
2020-04-02 18:05:15 +0200

    homework

--- script01.py
+++ script01.py
@@ -9,23 +9,4 @@ def funkcja(a,b,c):
     else:
         dif = float(a) - float(b)
         print (dif)
-
 funkcja(a,b,c)
-
-
-
-
-
-
-
-
-
-#definition of 'sum'
-
-#else:
-#definition of 'dif'
- #   dif = float(num2) - float(num1)
-  #  print('Oto twoja niepspodzianka, różnica drugiej i pierwszej liczby', dif)
-
-
-
